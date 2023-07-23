f4da2aa1f0138a079bf213e4e42fc4af3b5c30f6
MohitSaini90 <97613863+MohitSaini90@users.noreply.github.com>
2023-07-23 18:15:57 +0530

    Update Login.js

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -45,46 +45,52 @@ const Login = () => {
 
   return (
     <>
-      <Layout>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email Address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
-              value={email}
-              onChange={handleEmailChange}
-              required
-            />
+      <Layout title={"Login-Page"}>
+        <div className="outer-container">
+          <div className="form-container">
+            <form onSubmit={handleSubmit}>
+              <div className="form-outline mb-4">
+                <label htmlFor="email" className="form-label">
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                />
+              </div>
+              <div className="form-outline mb-4">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                />
+              </div>
+
+              <div className="row mb-4">
+                <div className="col">
+                  <Link to={"/forgot-password"}>Forgot Password?</Link>
+                  <Link to={"/verify-email"}>Verify Email</Link>
+                </div>
+              </div>
+
+              <button type="submit" className="btn btn-primary btn-block">
+                Login
+              </button>
+            </form>
           </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
-              value={password}
-              onChange={handlePasswordChange}
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Login
-          </button>
-          <Link className="btn btn-primary" to={"/forgot-password"}>
-            Forgot Password
-          </Link>
-          <Link className="btn btn-primary" to={"/verify-email"}>
-            Verify Email
-          </Link>
-        </form>
+        </div>
       </Layout>
     </>
   );
