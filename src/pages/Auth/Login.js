72e8e4f33db01f364edc3eddeb87ad7399b3e77d
MohitSaini90 <97613863+MohitSaini90@users.noreply.github.com>
2023-07-23 17:46:01 +0530

    Update Login.js

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -6,6 +6,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { AuthContext } from "../../context/auth";
 import { Link } from "react-router-dom";
+import "../../styles/authStyles.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -22,7 +23,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/login`, { email, password });
+      const res = await axios.post(`/api/v1/auth/login`, { email, password });
       if (res && res?.data?.success === false) {
         toast.error(res?.data?.message);
       } else if (res && res.data.success) {
@@ -45,56 +46,52 @@ const Login = () => {
 
   return (
     <>
-      <Layout>
-        <form onSubmit={handleSubmit}>
-          <div className="form-outline mb-4">
-            <label htmlFor="email" className="form-label">
-              Email Address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
-              value={email}
-              onChange={handleEmailChange}
-              required
-            />
-          </div>
-          <div className="form-outline mb-4">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
-              value={password}
-              onChange={handlePasswordChange}
-              required
-            />
-          </div>
+      <Layout title={"Login-Page"}>
+        <div className="outer-container">
+          <div className="form-container">
+            <form onSubmit={handleSubmit}>
+              <div className="form-outline mb-4">
+                <label htmlFor="email" className="form-label">
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                />
+              </div>
+              <div className="form-outline mb-4">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                />
+              </div>
 
-          <div class="row mb-4">
-     
-          <div class="col">
-       
-          <Link  to={"/forgot-password"}>
-            Forgot Password?
-          </Link>
-                <Link  to={"/verify-email"}>
-            Verify Email
-          </Link>
-    </div>
-  </div>
-                
-          <button type="submit" className="btn btn-primary btn-block">
-            Login
-          </button>
-   
-          
-        </form>
+              <div className="row mb-4">
+                <div className="col">
+                  <Link to={"/forgot-password"}>Forgot Password?</Link>
+                  <Link to={"/verify-email"}>Verify Email</Link>
+                </div>
+              </div>
+
+              <button type="submit" className="btn btn-primary btn-block">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
       </Layout>
     </>
   );
