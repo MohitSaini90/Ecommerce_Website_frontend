7c0066798b4389fd9bb892161cd178075eec915b
MohitSaini90 <97613863+MohitSaini90@users.noreply.github.com>
2023-07-23 23:11:32 +0530

    Update AllProduct.js

--- src/pages/Admin/AllProduct.js
+++ src/pages/Admin/AllProduct.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from "react";
 import AdminMenu from "../../components/layout/AdminMenu";
 import toast from "react-hot-toast";
 import axios from "axios";
-import { NavLink, Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import "../../styles/AdminProducts.css";
 const AllProduct = () => {
   const [products, setProducts] = useState([]);
 
@@ -29,30 +29,34 @@ const AllProduct = () => {
   return (
     <>
       <Layout title={"Dashboard-All Products"}>
-        <div className="row">
-          <div className="col-md-3">
-            <AdminMenu></AdminMenu>
-          </div>
-          <div className="col-md-9">
-            <div className="card w-7 p-3">All products</div>
-            <div className=" d-flex">
-              {products?.map((p) => (
-                <Link to={`/dashboard/admin/product/${p.slug}`} key={p._id}>
-                  <div>
-                    <div className="card m-2" style={{ width: "18rem" }}>
-                      <img
-                        className="card-img-top"
-                        src={`/api/v1/products/product-photo/${p._id}`}
-                        alt={p.name}
-                      />
-                      <div className="card-body">
-                        <h5 className="card-title">{p.name}</h5>
-                        <p className="card-text">{p.description}</p>
+        <div className="container-flui p-3 m-3">
+          <div className="row">
+            <div className="col-md-3">
+              <AdminMenu></AdminMenu>
+            </div>
+            <div className="col-md-9 product-container">
+              <div className="card-container">
+                <h2>Products</h2>
+                <div className="d-flex flex-wrap">
+                  {products?.map((p) => (
+                    <Link to={`/dashboard/admin/product/${p.slug}`} key={p._id}>
+                      <div className="product-card">
+                        <div className="card" style={{ width: "18rem" }}>
+                          <img
+                            className="card-img-top"
+                            src={`${process.env.REACT_APP_API}/api/v1/products/product-photo/${p._id}`}
+                            alt={p.name}
+                          />
+                          <div className="card-body">
+                            <h5 className="card-title">{p.name}</h5>
+                            <p className="card-text">{p.description}</p>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                </Link>
-              ))}
+                    </Link>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
