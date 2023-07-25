dc8af6d36af4760591cec2370137582ac1b86d00
MohitSaini90 <97613863+MohitSaini90@users.noreply.github.com>
2023-07-26 00:53:17 +0530

    Update CategoryPage.js

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -2,17 +2,29 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import useCategory from "../hooks/useCategory";
 import { Link } from "react-router-dom";
+import "../styles/CategoryPage.css";
 
 const CategoryPage = () => {
   const categories = useCategory();
+
   return (
     <Layout title={"All-categories"}>
       <div className="container">
-        <div className="row">
+        <div className="row py-3 px-2">
           {categories?.map((c) => (
-            <div className="col-md-6 mt-5 mb-3" key={c._id}>
-              <Link to={`/category/${c.slug}`} className="btn btn-warning">
-                {c.name}
+            <div className="col-md-4 col-sm-6" key={c._id}>
+              <Link to={`/category/${c.slug}`}>
+                <div
+                  className="card "
+                  id="categoryCard"
+                  style={{
+                    backgroundImage: `url(/images/wooden-background.jpg)`,
+                  }}
+                >
+                  <div className="card-body" id="categorycard-body">
+                    <h3 id="card-title">{c.name}</h3>
+                  </div>
+                </div>
               </Link>
             </div>
           ))}
