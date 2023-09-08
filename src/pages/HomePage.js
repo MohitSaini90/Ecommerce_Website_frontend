2e3a1c994a8cbfa2b1e10558bdbd8b7ca7d84432
MohitSaini90 <97613863+MohitSaini90@users.noreply.github.com>
2023-09-08 21:09:03 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -151,7 +151,7 @@ const HomePage = () => {
           </div>
         </div>
         <div className="col-md-9">
-          <h1 className="text-center">All products</h1>
+          
           <div className="d-flex flex-wrap">
             {products?.map((p) => (
               <div key={p._id}>
